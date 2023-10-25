9a59de36ce1d6e37326c2647daf35b39d2aff266
M1hkel <Mihkelmaspanov22@gmail.com>
2023-10-25 22:50:37 +0300

    sa

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def get_auth_header(token):
 def search_for_artist(token, artist_name):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
-    #otsin artisti nime ja otsin ainult ühte limitiga
+    #otsin artisti nime ja otsin ainult ühte limitiga, muidu tuleb liiga palju
     query = f"?q={artist_name}&type=artist&limit=1"
     
     query_url = url + query
@@ -47,7 +47,7 @@ def search_for_artist(token, artist_name):
 
 def get_songs_by_artist(token, artist_id):
     #lõpus annad riigi kust otsid top tracke
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=EE"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
     json_result = json.loads(result.content)["tracks"]
@@ -55,7 +55,7 @@ def get_songs_by_artist(token, artist_id):
 
 
 token = get_token()
-result = search_for_artist(token, "ACDC")
+result = search_for_artist(token, "mesabby")
 #artisti nimi
 #print(result["name"])
 
