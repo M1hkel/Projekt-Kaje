07639dbc6b34ec4d57fa9c5869f9a70eab7a2658
M1hkel <Mihkelmaspanov22@gmail.com>
2023-11-14 11:31:31 +0200

    uus playlist

--- app.py
+++ app.py
@@ -33,33 +33,11 @@ client_user_name = os.getenv("USER_NAME")
 redirect_uri = os.getenv("REDIRECT_URI")
 token = get_token()
 
+scope = 'playlist-modify-public playlist-modify-private user-library-read'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
 
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
-
-#search for artist and get top tracks
-def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    #otsin artisti nime ja otsin ainult ühte limitiga, muidu tuleb liiga palju
-    query = f"?q={artist_name}&type=artist&limit=1"
-    
-    query_url = url + query
-    result = get(query_url, headers = headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-    if len(json_result) == 0:
-        print("No artist with this name exists")
-        return None
-    return json_result[0]
-
-
-def get_songs_by_artist(token, artist_id):
-    #lõpus annad riigi kust otsid top tracke
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)["tracks"]
-    return json_result
 
 
 def get_auth(redirect_uri, client_id, client_secret):
@@ -78,38 +56,41 @@ def get_auth(redirect_uri, client_id, client_secret):
     if token_info:
         token = token_info['access_token']
 
-def print_playlists(client_id, client_secret, redirect_uri):
+def get_playlists(client_id, client_secret, redirect_uri):
     # Scope defines the permissions you need
-    scope = 'playlist-read-private playlist-read-collaborative'
+    #scope = 'playlist-read-private playlist-read-collaborative'
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
+    #sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
 
     # Now you have a Spotify instance with access to your playlists
-
+    playlistlst = []
     playlists = sp.current_user_playlists()
     for playlist in playlists['items']:
-        print(playlist['name'])
+        playlistlst.append(playlist['name'])
+    return playlistlst
 
 
-def get_playlist_tracks(playlist_name):
-    scope = 'playlist-read-private playlist-read-collaborative'
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
+
+def get_all_songs():   
+    songlst = []
+    kordus = 0
+    
     playlists = sp.current_user_playlists()
-    playlist_id = None
-    for playlist in playlists['items']:
-        if playlist['name'] == playlist_name:
-            playlist_id = playlist['id']
-            break
-    # Check if the playlist was found
-    if playlist_id:
-        # Retrieve the tracks within the playlist
-        playlist_tracks = sp.playlist_tracks(playlist_id)
+    
+    for i in playlists["items"]:
+        try:  
+            results = sp.playlist_tracks(i["id"])
+            j = 0
+            while (len(results["items"]) > 0):
+                for k in results["items"]:
+                    songlst.append(k["track"]['uri'])
+                    kordus += 1
+                j += 1
+                results = sp.playlist_tracks(i["id"], offset = j*100)
+        except:
+            pass
+    return songlst
 
-        # Print the track names
-        for track in playlist_tracks['items']:
-            print(track['track']['name'])
-    else:
-        print(f"Playlist '{playlist_name}' not found.")
 
 
 def get_listening_history():
@@ -121,36 +102,36 @@ def get_listening_history():
     return history
 
 
-def create_recommendation_playlist(client_id, client_secret, redirect_uri, playlist_name, num_tracks=10, target_seed_track=None):
+
+def create_recommendation_playlist(client_id, client_secret, redirect_uri, num_tracks=10, target_seed_track=None):
     # Scope defines the permissions your app needs
-    scope = 'playlist-modify-public playlist-modify-private user-library-read'
+    #scope = 'playlist-modify-public playlist-modify-private user-library-read'
 
     # Initialize the Spotify client with authentication
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
-
-    # Get the user's ID
-    user_id = sp.current_user()['id']
-
-    # Create a new playlist
-    #playlist = sp.user_playlist_create(user_id, playlist_name, public=True)
+    #sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
 
     # If a seed track is provided, use it to generate recommendations
     seed_tracks = [target_seed_track] if target_seed_track else []
     
     # Generate recommendations based on the seed tracks
+    
     recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=num_tracks)
-
+    
     # Extract the URIs of the recommended tracks
+    
     track_uris = [track['uri'] for track in recommendations['tracks']]
 
     return track_uris
-    
+
+
+
+
 #fuck yeah
 def create_recommendation_based_based_on_history():
     history = get_listening_history()
-    
-    scope = 'playlist-modify-public playlist-modify-private user-library-read'
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
+    all = get_all_songs()
+    #scope = 'playlist-modify-public playlist-modify-private user-library-read'
+    #sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
     user_id = sp.current_user()['id']
 
     playlist_name = "uus soovitus"
@@ -163,7 +144,10 @@ def create_recommendation_based_based_on_history():
         # Get the URI of the first track in the search results
         if results['tracks']['items']:
             target_seed_track = results['tracks']['items'][0]['uri']
-            
-            sp.user_playlist_add_tracks(user_id, playlist["id"], create_recommendation_playlist(client_id, client_secret, redirect_uri, playlist_name, num_tracks=2, target_seed_track=target_seed_track))
+            laulud = create_recommendation_playlist(client_id, client_secret, redirect_uri, num_tracks=2, target_seed_track=target_seed_track)
+            while laulud[0] in all or laulud[1] in all:
+                laulud = create_recommendation_playlist(client_id, client_secret, redirect_uri, num_tracks=2, target_seed_track=target_seed_track)
+            sp.user_playlist_add_tracks(user_id, playlist["id"], laulud)
 
 create_recommendation_based_based_on_history()
+
