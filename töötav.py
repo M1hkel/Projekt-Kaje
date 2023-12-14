cd6f76c1e08632d4446e4a5e984dcb19872c07dd
M1hkel <mihkelmaspanov22@gmail.com>
2023-12-14 14:11:14 +0200

    tee logo korda

--- töötav.py
+++ töötav.py
@@ -13,6 +13,114 @@ from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
+
+
+
+def get_token():
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("UTF-8")
+    auth_base64 = str(base64.b64encode(auth_bytes),"utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "Application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers = headers, data = data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+
+
+
+scope = 'playlist-modify-public playlist-modify-private user-library-read'
+
+
+def get_all_songs():  
+    
+    global sp
+    songlst = []
+    kordus = 0
+    
+    playlists = sp.current_user_playlists()
+    
+    for i in playlists["items"]:
+        try:  
+            results = sp.playlist_tracks(i["id"])
+            j = 0
+            while (len(results["items"]) > 0):
+                for k in results["items"]:
+                    songlst.append(k["track"]['uri'])
+                    kordus += 1
+                j += 1
+                results = sp.playlist_tracks(i["id"], offset = j*100)
+        except:
+            pass
+    return songlst
+
+
+
+def get_listening_history():
+    global sp
+    # Fetch your recently played tracks
+    history = sp.current_user_recently_played()
+    return history
+
+
+
+def create_recommendation_playlist(client_id, client_secret, redirect_uri, num_tracks=10, target_seed_track=None):
+
+    # If a seed track is provided, use it to generate recommendations
+    seed_tracks = [target_seed_track] if target_seed_track else []
+    
+    # Generate recommendations based on the seed tracks
+    
+    recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=num_tracks)
+    
+    # Extract the URIs of the recommended tracks
+    
+    track_uris = [track['uri'] for track in recommendations['tracks']]
+
+    return track_uris
+
+
+
+
+#fuck yeah
+def create_recommendation_based_on_history():
+
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    redirect_uri = os.getenv("REDIRECT_URI")
+    global sp
+    history = get_listening_history()
+    all = get_all_songs()
+    
+    user_id = sp.current_user()['id']
+
+    playlist_name = "uus soovitus"
+    playlist_name = "Your song suggestions"
+
+    playlist = sp.user_playlist_create(user_id, playlist_name, public=True)
+    playlist = sp.user_playlist_create(user_id, playlist_name, public=True, description="This suggestion playlist was made with Mihkel Maspanov's Spotify API app")
+    for item in history.get("items", []):
+        track_name = item.get("track", {}).get("name")
+        results = sp.search(q=track_name, type='track')
+
+        # Get the URI of the first track in the search results
+        if results['tracks']['items']:
+            target_seed_track = results['tracks']['items'][0]['uri']
+            laulud = create_recommendation_playlist(client_id, client_secret, redirect_uri, num_tracks=2, target_seed_track=target_seed_track)
+            #kontrrolli, et laule ei ole playlistides
+            while laulud[0] in all or laulud[1] in all:
+                laulud = create_recommendation_playlist(client_id, client_secret, redirect_uri, num_tracks=2, target_seed_track=target_seed_track)
+            sp.user_playlist_add_tracks(user_id, playlist["id"], laulud)
+
+
 class myGui:
     def __init__(self):
         self.root = tk.Tk()
@@ -97,6 +205,9 @@ class myGui:
         label.pack(padx=10, pady=10)
 
     def get_entry_values(self):
+        client_id = os.getenv("CLIENT_ID")
+        client_secret = os.getenv("CLIENT_SECRET")
+        redirect_uri = os.getenv("REDIRECT_URI")
         # Get the values entered in the Entry widgets
         user_name = self.entry1.get()
         os.environ["USER_NAME"] = user_name
@@ -104,11 +215,15 @@ class myGui:
         os.environ["CLIENT_ID"] = client_id
         client_secret = self.entry3.get()
         os.environ["CLIENT_SECRET"] = client_secret
+        global sp
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope='playlist-modify-public playlist-modify-private user-library-read user-read-recently-played', requests_timeout=20))
+        
 
-        # Use the values as needed (for example, print them)
-        print(f"User Name: {user_name}")
-        print(f"Client ID: {client_id}")
-        print(f"Client Secret: {client_secret}")
+        token = get_token()
+        create_recommendation_based_on_history()
+
+
+        
 
         messagebox.showinfo(title = "Sõnum", message = "Su muusika ootab sind, mine otsi Spotify'st playlist nimega uus soovitus.")
 
@@ -128,108 +243,11 @@ myGui()
 
 
 
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("UTF-8")
-    auth_base64 = str(base64.b64encode(auth_bytes),"utf-8")
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "Application/x-www-form-urlencoded"
-    }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers = headers, data = data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
-
-
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-client_user_name = os.getenv("USER_NAME")
-redirect_uri = os.getenv("REDIRECT_URI")
-
-
-token = get_token()
-
-scope = 'playlist-modify-public playlist-modify-private user-library-read'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope, requests_timeout=20))
-
-
-def get_all_songs():   
-    songlst = []
-    kordus = 0
-    
-    playlists = sp.current_user_playlists()
-    
-    for i in playlists["items"]:
-        try:  
-            results = sp.playlist_tracks(i["id"])
-            j = 0
-            while (len(results["items"]) > 0):
-                for k in results["items"]:
-                    songlst.append(k["track"]['uri'])
-                    kordus += 1
-                j += 1
-                results = sp.playlist_tracks(i["id"], offset = j*100)
-        except:
-            pass
-    return songlst
-
-
-
-def get_listening_history():
-    # Initialize Spotipy
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri, scope='user-read-recently-played'))
-
-    # Fetch your recently played tracks
-    history = sp.current_user_recently_played()
-    return history
-
-
-
-def create_recommendation_playlist(client_id, client_secret, redirect_uri, num_tracks=10, target_seed_track=None):
-    
-    # If a seed track is provided, use it to generate recommendations
-    seed_tracks = [target_seed_track] if target_seed_track else []
-    
-    # Generate recommendations based on the seed tracks
-    
-    recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=num_tracks)
-    
-    # Extract the URIs of the recommended tracks
-    
-    track_uris = [track['uri'] for track in recommendations['tracks']]
-
-    return track_uris
 
 
 
 
-#fuck yeah
-def create_recommendation_based_on_history():
-    history = get_listening_history()
-    all = get_all_songs()
-    
-    user_id = sp.current_user()['id']
 
-    playlist_name = "uus soovitus"
-    playlist_name = "Your song suggestions"
 
-    playlist = sp.user_playlist_create(user_id, playlist_name, public=True)
-    playlist = sp.user_playlist_create(user_id, playlist_name, public=True, description="This suggestion playlist was made with Mihkel Maspanov's Spotify API app")
-    for item in history.get("items", []):
-        track_name = item.get("track", {}).get("name")
-        results = sp.search(q=track_name, type='track')
 
-        # Get the URI of the first track in the search results
-        if results['tracks']['items']:
-            target_seed_track = results['tracks']['items'][0]['uri']
-            laulud = create_recommendation_playlist(client_id, client_secret, redirect_uri, num_tracks=2, target_seed_track=target_seed_track)
-            #kontrrolli, et laule ei ole playlistides
-            while laulud[0] in all or laulud[1] in all:
-                laulud = create_recommendation_playlist(client_id, client_secret, redirect_uri, num_tracks=2, target_seed_track=target_seed_track)
-            sp.user_playlist_add_tracks(user_id, playlist["id"], laulud)
 
-create_recommendation_based_on_history()
